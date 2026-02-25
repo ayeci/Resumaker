23f6bd2765a1a2ef1852933513644578bf027a01
ayeci <tatsuya@yamakawa.tokyo>
2026-02-25 16:12:17 +0900

    feat: PWA化準備

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'web-app-manifest-192x192.png', 'web-app-manifest-512x512.png'],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+      },
+      manifest: {
+        "name": "Resumaker",
+        "short_name": "Resumaker",
+        "icons": [
+          {
+            "src": "/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone"
+      }
+    })
+  ],
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
